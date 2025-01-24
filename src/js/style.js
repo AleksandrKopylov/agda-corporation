bc36f47aac18275c1bc9682f7542d3aa8ea730fe
Alex <grandekhv@gmail.com>
2025-01-24 10:34:03 +0100

    Works nemu hamburger on JS

--- src/js/style.js
+++ src/js/style.js
@@ -1,3 +1,17 @@
+//menu hamburger
+
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.head__menu'),
+      closeElem = document.querySelector('.head__close');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.add('active');  
+});   
+
+closeElem.addEventListener('click', () => {
+  menu.classList.remove('active');  
+});  
+
 //lightbox
 
 function openModal() {
